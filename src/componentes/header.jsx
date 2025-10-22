cdc8b0f8bafa1ef538e825e962d82bf6e0bcf9f7
Ariadna Bertolini <ariadnabertolini@gmail.com>
2025-10-22 11:57:05 -0300

    creamos el header.jsx y el header.css

---
+++ src/componentes/header.jsx
@@ -0,0 +1,8 @@
+export default function Header(){
+    return(
+        <header>
+            <h1>clases react cfl 414</h1>
+            <p>lunes, miercoles y viernes de 9:10 a 12:00</p>
+        </header>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,15 @@
+//imports dependencias
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+//imports componentes
+import Header from './componentes/header'
 import Inicio from './componentes/inicio'
+//imports css
+import './css/general.css' 
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Header />
     <Inicio />
   </StrictMode>,
 )
