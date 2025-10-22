b709b143eb4b5aa18750b7874e182d09b24a0d80
Ariadna Bertolini <ariadnabertolini@gmail.com>
2025-10-22 11:04:09 -0300

    commit inicial

---
+++ src/componentes/Inicio.jsx
@@ -0,0 +1,13 @@
+export default function Inicio(){
+    //este es el cuerpo basico de la funcion a exportar
+    //estas funciones son las que llamamos componentes
+    //los nombres de estos componentes deben iniciar con una mayuscula, siguiendo el formato UpperCamelCase
+    //para rendedizar el html dentro de estas funciones, tenemos que escribirlo como argumento del return
+    return(
+        <div>
+          <h2>Mi primer titulo</h2>
+          <p>ya tenemos creada nuestra primera app en react. este documento sera editado multiples veces, es un buen momento para crear el repositorio de git</p>
+        
+        </div>
+    )
+}
